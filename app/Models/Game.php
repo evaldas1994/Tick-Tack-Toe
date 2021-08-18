7fdbb1bd5ce9e12876333ba530a98fc3a9f830e0
Evaldas1994 <evaldas.tuleikis@email.com>
2021-08-18 21:42:30 +0300

    Added game model relations (user1, user2, box)

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Game extends Model
 {
@@ -21,4 +22,28 @@ class Game extends Model
         'user2_id',
         'box_id'
     ];
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function user1(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user1_id');
+    }
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function user2(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user2_id');
+    }
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function box(): HasOne
+    {
+        return $this->hasOne(Box::class, 'id', 'box_id');
+    }
 }
